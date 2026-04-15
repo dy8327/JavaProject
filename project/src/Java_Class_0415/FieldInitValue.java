21593e6224d6d1cf0df0147897fb7fa3e15dba87
DoYoung Ryu <dy8327@gmail.com>
2026-04-15 10:19:40 +0900

    Field초기화 Class

---
+++ project/src/Java_Class_0415/FieldInitValue.java
@@ -0,0 +1,19 @@
+package Java_Class_0415;
+
+public class FieldInitValue {
+    //필드
+    byte byteField;
+    short shortField;
+    int intField;
+    long longField;
+
+    boolean booleanField;
+    char charField;
+
+    float floatField;
+    double doubleField;
+
+    int[] arrField;
+    String referenceField;
+
+}
