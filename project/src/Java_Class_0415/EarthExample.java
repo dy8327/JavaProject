d1014586359b364aa9fe6f3b1cac0125765b0235
DoYoung Ryu <dy8327@gmail.com>
2026-04-15 16:58:12 +0900

    상수예제_실행파일

---
+++ project/src/Java_Class_0415/EarthExample.java
@@ -0,0 +1,8 @@
+package Java_Class_0415;
+
+public class EarthExample { //상수예제
+    public static void main(String[] args) {
+        System.out.println("지구의 반지름: "+Earth.EARTH_RADIUS+"km");
+        System.out.println("지구의 표면적: "+Earth.EARTH_SURFACE_AREA+"km^2");
+    }
+} //다시확인
