512764b70ec9419178363a9ca65d64067f0fc914
DoYoung Ryu <dy8327@gmail.com>
2026-04-15 11:09:30 +0900

    Korean_Class

---
+++ project/src/Java_Class_0415/Korean.java
@@ -0,0 +1,14 @@
+package Java_Class_0415;
+
+public class Korean {
+    // 필드
+    String nation ="대한민국";
+    String name;
+    String ssn;
+
+    //생성자
+    public Korean(String name, String ssn){
+        this.name=name;
+        this.ssn=ssn;
+    }
+}
