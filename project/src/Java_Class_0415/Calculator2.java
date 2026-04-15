defe3d38eb303c77eb2a5bfe1c18cbc516e7da67
DoYoung Ryu <dy8327@gmail.com>
2026-04-15 16:04:31 +0900

    정적멤버_Class

---
+++ project/src/Java_Class_0415/Calculator2.java
@@ -0,0 +1,13 @@
+package Java_Class_0415;
+
+public class Calculator2 { //정적멤버 사용
+
+    static double pi = 3.14159;
+    static int plus(int x, int y){
+        return x+y;
+    }
+
+    static int minus(int x, int y){
+        return x-y;
+    }
+}
