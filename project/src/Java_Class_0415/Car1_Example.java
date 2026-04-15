63e418fd81f16750795d80025690bd8a7ddc37ac
DoYoung Ryu <dy8327@gmail.com>
2026-04-15 11:01:20 +0900

    Car1실행파일_Car의 주소확인

---
+++ project/src/Java_Class_0415/Car1_Example.java
@@ -0,0 +1,10 @@
+package Java_Class_0415;
+
+public class Car1_Example {
+    public static void main(String[] args) {
+        Car1 myCar1 = new Car1("검정", 3000);
+
+        System.out.println(myCar1);
+       // System.out.println(myCar1.color);
+    }
+}
