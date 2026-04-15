9211f94244617f3c0c5e99acd2470d2bf7ce037e
DoYoung Ryu <dy8327@gmail.com>
2026-04-15 14:23:24 +0900

    메서드 호출방법_실행파일

---
+++ project/src/Java_Class_0415/Calculator1Example.java
@@ -0,0 +1,8 @@
+package Java_Class_0415;
+
+public class Calculator1Example {
+    public static void main(String[] args) {
+        Calculator1 myCal1 = new Calculator1();
+        myCal1.execute();
+    }
+}
