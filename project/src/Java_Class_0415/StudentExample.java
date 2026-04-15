5248fc126eea2f250f7dc02d53dc20532517df3b
DoYoung Ryu <dy8327@gmail.com>
2026-04-15 10:20:27 +0900

    Student 실행파일

---
+++ project/src/Java_Class_0415/StudentExample.java
@@ -0,0 +1,15 @@
+package Java_Class_0415;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        System.out.println(s1);
+
+        Student s2 = new Student();
+        System.out.println(s2);
+    }
+}
+
+//Java_Class_0415.Student@3f102e87
+//Java_Class_0415.Student@27abe2cd
+//s1과 s2가 점유한 메모리의 주소가 나온다.
