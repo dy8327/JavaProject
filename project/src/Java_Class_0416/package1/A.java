34dc5f9e354053f8c573b8b2927cbc7504b44b57
DoYoung Ryu <dy8327@gmail.com>
2026-04-16 17:16:09 +0900

    오류수정

--- project/src/Java_Class_0416/package1/A.java
+++ project/src/Java_Class_0416/package1/A.java
@@ -4,13 +4,13 @@ public class A {
     //필드
     public int field1; //공용사용가능
     int field2; //기본은 defult이고 자식에게만 사용권한을 준다.
-    private int field3; //현재 클래스 내에서만 사용가능
+    //private int field3; //현재 클래스 내에서만 사용가능
 
     //생성자
     public A(){
         field1 =1;
         field2 =1;
-        field3 =1;
+       // field3 =2;
 
         method1();
         method2();
