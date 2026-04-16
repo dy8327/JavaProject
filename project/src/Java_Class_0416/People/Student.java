fb83bb37f18f4b8c03c3e2b7275750d97cd179d6
DoYoung Ryu <dy8327@gmail.com>
2026-04-16 12:05:56 +0900

    주석추가

--- project/src/Java_Class_0416/People/Student.java
+++ project/src/Java_Class_0416/People/Student.java
@@ -4,7 +4,7 @@ public class Student extends People{
     public int StudentNo;
 
     public Student(String name, String ssn, int StudentNo){
-        super(name, ssn);
+        super(name, ssn); //부모생성자 호출(부모으 생성자가 없을땐 사용 금지), 자식생성자의 제일 첫줄에 작성!!
         this.StudentNo = StudentNo;
     }
 }
