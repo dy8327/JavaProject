a11a5a69f939680b79ddfc287fec89f0a4191095
DoYoung Ryu <dy8327@gmail.com>
2026-04-16 17:02:02 +0900

    코드 완료

--- project/src/Java_DataType_0414/ArrayInArray.java
+++ project/src/Java_DataType_0414/ArrayInArray.java
@@ -13,6 +13,19 @@ public class ArrayInArray {
         int[][] engScores = new int[2][];
         engScores[0] = new int[2]; //engScores의 0인덱스는 2열을 가진다.
         engScores[1] = new int[3]; //engScores의 1인덱스는 3열을 가진다.
+        for(int i=0; i<engScores.length;i++){
+            for(int k=0; k<engScores[i].length;k++){
+                System.out.println("engScore["+i+"]["+k+"]="+engScores[i][k]);
+            }
+        }
+        System.out.println();
+
+        int[][] javaScores={{95,80},{92,96,80}};
+        for(int i=0;i<javaScores.length;i++){
+            for(int k=0;k<javaScores[i].length;k++){
+                System.out.println("javaScores["+i+"]["+k+"]="+javaScores[i][k]);
+            }
+        }
 
     }
 }
