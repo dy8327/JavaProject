bbd0939cfe1257c9bdb71a265e103710984877e9
DoYoung Ryu <dy8327@gmail.com>
2026-04-16 12:23:19 +0900

    오버라이딩예제_자식Class

---
+++ project/src/Java_Class_0416/Computer/Computer.java
@@ -0,0 +1,9 @@
+package Java_Class_0416.Computer;
+
+public class Computer extends Calculator{
+    @Override
+    double areaCircle(double r){
+        System.out.println("Computer 객체의 areaCircle()실행");
+        return Math.PI *r*r;
+    }
+}
