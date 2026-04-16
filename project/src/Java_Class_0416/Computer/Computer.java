71e12b8f981456cf6bb5fef8cc4986b33858a9a2
DoYoung Ryu <dy8327@gmail.com>
2026-04-16 12:24:34 +0900

    주석 추가

--- project/src/Java_Class_0416/Computer/Computer.java
+++ project/src/Java_Class_0416/Computer/Computer.java
@@ -1,8 +1,8 @@
 package Java_Class_0416.Computer;
 
 public class Computer extends Calculator{
-    @Override
-    double areaCircle(double r){
+    @Override //오버라이딩 명령어
+    double areaCircle(double r){ //부모의 형식과 같아야 한다.
         System.out.println("Computer 객체의 areaCircle()실행");
         return Math.PI *r*r;
     }
