62692df3f5127369a790aa608f613a969470e8f8
DoYoung Ryu <dy8327@gmail.com>
2026-04-16 16:40:03 +0900

    다형성_자동타입변환_자식Class

---
+++ project/src/Java_Class_0416/Child/Child.java
@@ -0,0 +1,11 @@
+package Java_Class_0416.Child;
+
+public class Child extends Parent{
+    @Override
+    public void method2(){
+        System.out.println("Child-method2()");
+    }
+    public void method3(){
+        System.out.println("Child-method3()");
+}
+}
