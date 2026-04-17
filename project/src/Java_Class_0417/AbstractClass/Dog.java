4f7b8c4a7d638193205e8635411ba5598feb348b
DoYoung Ryu <dy8327@gmail.com>
2026-04-17 17:08:19 +0900

    추상클래스_자식Class

---
+++ project/src/Java_Class_0417/AbstractClass/Dog.java
@@ -0,0 +1,11 @@
+package Java_Class_0417.AbstractClass;
+
+public class Dog extends Animal{
+    public Dog(){
+    this.kind ="포유류";
+    }
+    @Override
+    public void sound(){
+        System.out.println("멍멍");
+    }
+}
