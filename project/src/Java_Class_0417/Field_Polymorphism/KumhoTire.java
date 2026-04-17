14d372898aff1a2b8b9dd054a60afb936fd36d63
DoYoung Ryu <dy8327@gmail.com>
2026-04-17 11:37:43 +0900

    필드의 다형성_금호타이어_자식Class

---
+++ project/src/Java_Class_0417/Field_Polymorphism/KumhoTire.java
@@ -0,0 +1,20 @@
+package Java_Class_0417.Field_Polymorphism;
+
+public class KumhoTire extends Tire{
+    //생성자
+    public KumhoTire(String location, int maxRotation){
+        super(location, maxRotation);
+    }
+    //메소드
+    @Override
+    public boolean roll(){
+        ++nowRotation;
+        if(nowRotation<maxRotation){
+            System.out.println(location+"KumhoTire 수명 : "+(maxRotation-nowRotation)+"회");
+            return true;
+        }else{
+            System.out.println("***"+location+"KumhoTire 펑크");
+            return false;
+        }
+    }
+}
