01575ed83c608ab5a9e1ce055a34c5a9543c47f0
DoYoung Ryu <dy8327@gmail.com>
2026-04-17 17:10:32 +0900

    인터페이스_매개변수다형성_자식Class

---
+++ project/src/Java_Class_0417/Interface_Parameter_Polymorphism/Taxi.java
@@ -0,0 +1,9 @@
+package Java_Class_0417.Interface_Parameter_Polymorphism;
+
+public class Taxi implements Vehicle{
+    @Override
+    public void run(){
+        System.out.println("택시가 달립니다.");
+    }
+    
+}
