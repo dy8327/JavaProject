64f5ef3126d7e7c56752a1d24b7b04d848500636
DoYoung Ryu <dy8327@gmail.com>
2026-04-17 17:21:25 +0900

    작동 추가

--- project/src/Java_Class_0417/RemoteControl/RemoteControlExample.java
+++ project/src/Java_Class_0417/RemoteControl/RemoteControlExample.java
@@ -4,8 +4,11 @@ public class RemoteControlExample {
     public static void main(String[] args) {
         RemoteControl rc;
         rc = new SmartTelevision();
+        rc.turnOn();
+        rc.turnOff();
         rc = new Audio();
-
+        rc.turnOn();
+        rc.turnOff();
         RemoteControl.ChangeBattery();
     }
 }
