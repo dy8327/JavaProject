13cac4db01ac261b88beca7d861ae81e19534bb3
DoYoung Ryu <dy8327@gmail.com>
2026-04-17 11:36:50 +0900

    필드의 다형성_실행클래스

---
+++ project/src/Java_Class_0417/Field_Polymorphism/CarExample.java
@@ -0,0 +1,30 @@
+package Java_Class_0417.Field_Polymorphism;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        for(int i=1; i<=5; i++){
+            int problemLocation = car.run(); //run을 5번 실행
+            switch(problemLocation){
+                case 1:
+                System.out.println("앞왼쪽 한국타이어로 교체");
+                car.frontLeftTire = new HankookTire("앞왼쫏", 15);
+                break;
+                case 2:
+                System.out.println("앞오른쪽 금호타이어로 교체");
+                car.frontRightTire = new KumhoTire("앞오른쪽", 13);
+                break;
+                case 3:
+                System.out.println("뒤왼쪽 한국타이어로 교체");
+                car.rearLeftTire = new HankookTire("뒤왼쪽", 14);
+                break;
+                case 4:
+                System.out.println("뒤오른쪽 금호타이어로 교체");
+                car.rearRighTire = new KumhoTire("뒤오른쪽", 17);
+                break;
+            }
+        }
+        System.out.println("--------------------------------");
+    }
+}
