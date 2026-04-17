f77f176b779f08f44969f0bfbedd4e6350ba99b8
DoYoung Ryu <dy8327@gmail.com>
2026-04-17 16:51:08 +0900

    인터페이스간 상속_하위구현

---
+++ project/src/Java_Class_0417/InterFace_extends/ImplementationC.java
@@ -0,0 +1,15 @@
+package Java_Class_0417.InterFace_extends;
+
+public class ImplementationC implements InterfaceC{
+    public void methodA(){
+        System.out.println("ImplementationC-methodA() 실행");
+    }
+    public void methodB(){
+        System.out.println("ImplementationC-methodB() 실행");
+    }
+    public void methodC(){
+        System.out.println("ImplementationC-methodC() 실행");
+    }
+    
+}
+//추상클래스(인터페이스)를 실체클래스로 작업
