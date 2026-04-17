8426859cd7d9ee1a9a2c6b7515ec92020ce3c07b
DoYoung Ryu <dy8327@gmail.com>
2026-04-17 17:12:09 +0900

    매개변수다형성_활용클래스

---
+++ project/src/Java_Class_0417/Parameter_Polymorphism/DriverExample.java
@@ -0,0 +1,13 @@
+package Java_Class_0417.Parameter_Polymorphism;
+
+public class DriverExample {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+
+        Bus bus = new Bus();
+        Taxi taxi = new Taxi();
+
+        driver.drive(bus);
+        driver.drive(taxi);
+    }
+}
