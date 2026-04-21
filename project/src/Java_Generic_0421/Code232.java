69ba7919c36c5a13d2dd5ea758fb37085bf0c5f2
DoYoung Ryu <dy8327@gmail.com>
2026-04-21 16:57:19 +0900

    와일드카드_Class

---
+++ project/src/Java_Generic_0421/Code232.java
@@ -0,0 +1,23 @@
+package Java_Generic_0421;
+
+class WithWild<T extends Number>{
+    T data;
+    WithWild(T d){data =d;}
+    boolean same(WithWild<?> x){
+        if (Math.abs(data.doubleValue())==Math.abs(x.data.doubleValue()))
+            return true;
+            return false;
+    }
+}
+public class Code232 {
+    public static void main(String[] args) {
+        WithWild<Integer> a = new WithWild<Integer>(6);
+        WithWild<Double> b = new WithWild<Double>(-6.0);
+        WithWild<Long> c = new WithWild<Long>(5L);
+        if(a.same(b)) System.out.println("a와 b는 같습니다.");
+        else System.out.println("a와 b는 다릅니다.");
+        if(a.same(c)) System.out.println("a와 c는 같습니다.");
+        else System.out.println("a와 c는 다릅니다.");
+        
+    }
+}
