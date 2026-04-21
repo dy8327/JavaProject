24705413cd11529661ba463bded6bfefbd8ebf72
DoYoung <dy8327@gmail.com>
2026-04-22 03:48:27 +0900

    멈춤

--- project/src/Java_Project_0421/Manageable.java
+++ project/src/Java_Project_0421/Manageable.java
@@ -2,10 +2,12 @@ package Java_Project_0421;
 
 public interface Manageable {
       
+    String bookid = null;
     //추상메소드
-    void addItem(); //데이터 추가 
+    //void addItem(Book book); //데이터 추가 
     void displayAll();
     void saveToFile();
     void loadFromFile();
+    void addItem();
 
 }
