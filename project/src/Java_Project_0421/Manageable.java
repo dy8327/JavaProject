9e5433b3793d9172853593e7dfa2d2775e8991c5
DoYoung Ryu <dy8327@gmail.com>
2026-04-21 17:34:21 +0900

    자바파이널프로젝트_최상위인터페이스

---
+++ project/src/Java_Project_0421/Manageable.java
@@ -0,0 +1,11 @@
+package Java_Project_0421;
+
+public interface Manageable {
+      
+    //추상메소드
+    void addItem(); //데이터 추가 
+    void displayAll();
+    void saveToFile();
+    void loadFromFile();
+
+}
