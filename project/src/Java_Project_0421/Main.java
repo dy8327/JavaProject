2e2fe9fab92beabc5e69fee723f2220e7f12c86a
DoYoung <dy8327@gmail.com>
2026-04-22 03:48:10 +0900

    멈춤

--- project/src/Java_Project_0421/Main.java
+++ project/src/Java_Project_0421/Main.java
@@ -1,7 +1,26 @@
 package Java_Project_0421;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String[] Book = 
+        //Book book = new Book();
+        Book book = new Book();
+        
+        //초기화면
+        Scanner mainsc = new Scanner(System.in);
+        System.out.println("===도서관 시스템===");
+        System.out.println("1. 도서 등록");
+        System.out.println("2. 도서 목록 조회");
+        System.out.println("3. 저장 및 종료");
+        System.out.print("메뉴 선택 : ");
+        int ms = mainsc.nextInt();
+        if(ms==1){
+        book.addItem();
+        System.out.println("도서아이디는"+book.getBookid);
+        System.out.print("성공적으로 등록되었습니다.");
+        }
+
+
     }
 }
