efdec8a53b548e36c49c8dad69c81365f59c786d
DoYoung Ryu <dy8327@gmail.com>
2026-04-22 12:50:32 +0900

    에러남 ㅜㅜ

--- project/src/Lib_Project_0421/BookExample.java
+++ project/src/Lib_Project_0421/BookExample.java
@@ -12,15 +12,25 @@ public class BookExample {
         System.out.println("===도서관 시스템===");
         System.out.println("1. 도서 등록");
         System.out.println("2. 도서 목록 조회");
-        System.out.println("3. 저장 및 종료");
+        System.out.println("3. 종료");
         System.out.print("메뉴 선택 : ");
         int ms = mainsc.nextInt();
+        
         if(ms==1){
         book.addItem();
-        } else if(ms==2){
-            book.loadFromFile();
-        } else if(ms==3){
+        System.out.println("저장하시겠습니까?(y/n)");
+        String ms1 = mainsc.next();
+           switch(ms1){
+            case "y", "Y":
             book.saveToFile();
+            continue;
+            case "n", "N":
+            System.out.println("저장을 취소하였습니다.");
+            continue;
+           }
+        } else if(ms==2){
+            book.displayAll();
+        } else if(ms==3){
             break;
         }
     }
